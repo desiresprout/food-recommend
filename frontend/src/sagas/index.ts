6b71929af213cef2afff73ec38ce414633c7c32b
mara <mara@iMac.local>
2020-10-30 16:10:32 +0900

    remove unless source code

--- frontend/src/sagas/index.ts
+++ frontend/src/sagas/index.ts
@@ -1,10 +1,6 @@
 import axios from "axios";
 import { all } from "redux-saga/effects";
-import { baseUrl } from "../config/config";
 import foodSaga from "@sagas/food";
-
-axios.defaults.baseURL = baseUrl;
-axios.defaults.withCredentials = true;
 
 function* rootSaga() {
   yield all([foodSaga()]);
